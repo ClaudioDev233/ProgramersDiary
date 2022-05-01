269694f6fc576e43235b29908d191b2aab83198f
Claudio <claudio.junior233@gmail.com>
2022-05-01 17:15:36 -0300

    feat: Criação da Entidade Compartilhada e Adicção das propriedades da entidade Cards

--- ProgrammersDiary.Domain/Entities/Card.cs
+++ ProgrammersDiary.Domain/Entities/Card.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProgrammersDiary.Domain.Entities.Shared;
 
 namespace ProgrammersDiary.Domain.Entities
 {
-    public class Card
+    public class Card : Entity
     {
-
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int LinguagemId { get; set; }
+        public Linguagem Linguagem { get; set; }
     }
 }
