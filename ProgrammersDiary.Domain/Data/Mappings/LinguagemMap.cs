3aafed70a85c80ba34fda760cb1eb72d691068e1
AndreMotta25 <andremotta16041998@gmail.com>
2022-05-01 23:30:32 -0300

    Feat: Fazendo o mapeamento das entidades

--- ProgrammersDiary.Domain/Data/Mappings/LinguagemMap.cs
+++ ProgrammersDiary.Domain/Data/Mappings/LinguagemMap.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersDiary.Domain.Entities;
 
 namespace ProgrammersDiary.Domain.Data.Mappings
 {
-    public class LinguagemMap
+    public class LinguagemMap : IEntityTypeConfiguration<Linguagem>
     {
-        
+        public void Configure(EntityTypeBuilder<Linguagem> builder)
+        {
+            builder.HasKey(linguagem => linguagem.Id);
+
+            builder.Property(linguagem => linguagem.Nome)
+            .HasColumnType("varchar(100)");
+
+            builder.Property(linguagem => linguagem.LabelLinguagem).HasColumnType("varchar(50)");
+        }
     }
 }
