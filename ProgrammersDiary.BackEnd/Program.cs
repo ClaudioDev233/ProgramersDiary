9fd44ef8e6a06ebbb31818c3904146178047c886
AndreMotta25 <andremotta16041998@gmail.com>
2022-05-01 23:24:12 -0300

    Fix: Concertando a injeção

--- ProgrammersDiary.BackEnd/Program.cs
+++ ProgrammersDiary.BackEnd/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Andre")));
 
 builder.Services.AddScoped<ICardService, CardService>();
 var app = builder.Build();
